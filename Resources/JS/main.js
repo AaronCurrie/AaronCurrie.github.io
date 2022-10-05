22153ec09944ea9d20f4150e003a9313b8ee3320
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-10-05 16:44:53 +0100

    updated skills

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -2,6 +2,16 @@
 const menuButton = document.getElementById("mobile-menu");
 const mobileMenu = document.getElementById("mobile-nav");
 
+const mobileLinks = [...document.querySelectorAll('.mobileLink')]
+
+mobileLinks.forEach(mobileLink => {
+    mobileLink.addEventListener('click', (e) => {
+        mobileMenu.classList.remove('active');
+        mobileMenu.classList.add('hidden');
+        menuButton.innerHTML = "Menu"   
+    })
+})
+
 menuButton.addEventListener("click", (e) => {
     if(mobileMenu.classList.contains('hidden')) {
         mobileMenu.classList.remove('hidden');
@@ -13,3 +23,4 @@ menuButton.addEventListener("click", (e) => {
         menuButton.innerHTML = "Menu"; 
     }   
 })
+
