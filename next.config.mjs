bf3ef31b2d757ef0b098ae84a26b6c238cdcdcd5
Aaron Currie <baron_currie@hotmail.com>
2025-06-25 16:42:30 +0100

    asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
+    reactStrictMode: true,
     output: 'export',
+    assetPrefix: './',
 };
 
 export default nextConfig;
